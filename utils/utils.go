173832a7b342dffae085238c6f2772943752dff4
Toby Latin-Stoermer <tlatin@gmail.com>
2015-05-06 00:44:37 -0700

    implementing utils and adding unit tests for them

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,31 @@
 package utils
 
-// TODO: fix for unit tests.
+import (
+  "os"
+  "strings"
+)
+
+var rootPackageName = "simpletimeline"
+
 func GetTemplatePath() (path string){
-	return "templates/"
+  path, err := os.Getwd()
+  if err != nil {
+    return "templates/"  
+  }
+
+  return strings.Repeat("../", GetRootPackageOffset(path)) + "app/templates/"
+}
+
+func GetRootPackageOffset(path string) (offset int) {
+  rootOffset := 0
+  dirs := strings.Split(path, "/")
+  for i, dir := range dirs {
+     rootOffset = i + 1
+     if dir == rootPackageName {
+      break
+     }
+  }
+
+  return len(dirs) - rootOffset
+
 }
---
+++ utils/utils_test.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+  "testing"
+  "strconv"
+)
+
+
+func TestGetRootPackageOffset(t *testing.T) { 
+  base := "/Users/tlatins/gocode/src/github.com/tlatin/"
+  offset := GetRootPackageOffset(base + rootPackageName)
+  if 0 != offset {
+        t.Error("Root Package returned the wrong offset. Expected: 2, Saw: " + strconv.Itoa(offset))
+  }
+  offset = GetRootPackageOffset(base + rootPackageName + "/controller/cron") 
+  if 2 != offset {
+        t.Error("Root Package returned the wrong offset. Expected: 2, Saw: " + strconv.Itoa(offset))
+  }
+}
