a6237ce04963665ce3006185c9f3e21818dec331
Toby Latin-Stoermer <tlatin@gmail.com>
2015-05-18 14:39:59 -0700

    adding min repro case for unit test issue

---
+++ minrepro/minrepro.go
@@ -0,0 +1,28 @@
+package Minrepro
+
+import (
+	"appengine"
+	"appengine/datastore"
+)
+
+type Parent struct {
+	Name string
+}
+
+type Child struct {
+	Name string
+}
+
+func NewParent(c appengine.Context, name string) (key *datastore.Key, err error) {
+	parent := Parent{
+		Name: name,
+	}
+	return datastore.Put(c, datastore.NewIncompleteKey(c, "Parent", nil), &parent)
+}
+
+func NewChild(c appengine.Context, parent *datastore.Key, name string) (key *datastore.Key, err error) {
+	child := Child{
+		Name: name,
+	}
+	return datastore.Put(c, datastore.NewIncompleteKey(c, "Parent", parent), &child)
+}
